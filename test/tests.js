a7c4b85c5fc7f9bf404ba549545992e8c13bc246
Jared Forsyth <jared@jaredforsyth.com>
2013-06-05 21:37:48 -0600

    adding tests

---
+++ test/tests.js
@@ -0,0 +1,58 @@
+
+var expect = require('chai').expect;
+var keys = require('../');
+
+var keynaming = [
+  ['create a name correctly', {keyCode: 32}, 'space'],
+  ['recognize alt',   {keyCode: 32, altKey: true},   'alt space'],
+  ['recognize ctrl',  {keyCode: 32, ctrlKey: true},  'ctrl space'],
+  ['recognize meta',  {keyCode: 32, metaKey: true},  'meta space'],
+  ['recognize shift', {keyCode: 32, shiftKey: true}, 'shift space'],
+  ['recognize ctrl+alt', {keyCode: 32, ctrlKey: true, altKey: true}, 'ctrl alt space'],
+];
+
+describe('keyname', function(){
+  keynaming.forEach(function(item){
+    it('should ' + item[0], function(){
+      expect(keys.keyname(item[1])).to.eql(item[2]);
+    });
+  });
+});
+
+describe('keys', function(){
+  keynaming.forEach(function(item){
+    it('should ' + item[0], function(){
+      var called = false;
+      var callb = function(){called=true;};
+      var config = {};
+      config[item[2]] = callb;
+      var fn = keys(config);
+      item[1].preventDefault = function(){};
+      item[1].stopPropagation = function(){};
+      fn(item[1]);
+      expect(called).to.be.true;
+    });
+  });
+});
+
+var normalizing = [
+  ['preserve single', 'A', 'A'],
+  ['ctrl', 'ctrl'],
+  ['ucase char', 'shift a', 'shift A'],
+  ['preserve good', 'ctrl shift space', 'ctrl shift space'],
+  ['order mods', 'shift ctrl space', 'ctrl shift space'],
+  ['fail unknown mod', 'a a', false],
+  ['fail unknown main', 'bad', false],
+  ['fail unknown main after mods', 'ctrl shift bad', false]
+];
+
+describe('normalize', function(){
+  normalizing.forEach(function(item){
+    var title = item.length == 3 ? item.shift() : 'normalize ' + item[0];
+    it('should ' + title, function(){
+      expect(keys.normalize(item[0])).to.eql(item[1]);
+    });
+  });
+});
+
+
